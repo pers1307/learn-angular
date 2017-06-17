2618e1c633e558f3f442ce3f165cef6f8a36a005
pers1307 <skulines@mail.ru>
2017-06-17 17:34:58 +0500

    hooks

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,13 +1,70 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {
+  Component, OnInit, Input, ViewEncapsulation, OnChanges, SimpleChanges, DoCheck,
+  AfterContentInit, AfterContentChecked, AfterViewChecked, AfterViewInit, OnDestroy, ViewChild, ElementRef, ContentChild
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.None // Отключить инкапсулирование стилей (убирается префиксы у стилей)
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewChecked,
+    AfterViewInit,
+    OnDestroy
+{
   @Input('srvElement')element: {type: string, name: string, content: string};
+  @Input()name: string;
+  @ViewChild('heading')header: ElementRef;
+  @ContentChild('contentParagraph')paragraph: ElementRef;
 
-  constructor() {}
-  ngOnInit() {}
+  constructor() {
+    console.log('constructor');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges');
+    console.log(changes);
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit');
+    console.log(this.header.nativeElement.textContent);
+    console.log(this.paragraph.nativeElement.textContent);
+  }
+
+  /**
+   * отлавливает любые изменения
+   */
+  ngDoCheck() {
+    console.log('ngDoCheck');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit');
+    console.log(this.paragraph.nativeElement.textContent);
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit');
+    console.log(this.header.nativeElement.textContent);
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
+  }
 }
