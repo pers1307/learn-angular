8176388a5515ecb9af36799bd57328efa9029fca
pers1307 <skulines@mail.ru>
2017-07-12 11:24:45 +0500

    observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UsersService} from "./users.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  user1Activated = false;
+  user2Activated = false;
 
-  public loadedFeature = 'recipe';
+  constructor(
+      private usersService: UsersService
+  ) {
 
-  public onNavigate(feature: string) {
-    this.loadedFeature = feature;
+  }
+
+  ngOnInit() {
+    this.usersService.userActivated.subscribe(
+        (id: number) => {
+          if (id === 1) {
+            this.user1Activated = true;
+          } else if (id === 2) {
+            this.user2Activated = true;
+          }
+        }
+    );
   }
 }
